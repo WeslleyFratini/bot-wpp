66a46526addb6b04829321aa9f6531c9e36e0872
unknown <weeslleey354@gmail.com>
2021-02-08 23:03:31 -0300

    criação do cardapio

---
+++ src/cardapio.js
@@ -0,0 +1,16 @@
+const cardapio = {
+  1: {
+    descricao: "X-Burguer",
+    preco: 10.0,
+  },
+  2: {
+    descricao: "X-Tudo",
+    preco: 15.0,
+  },
+  3: {
+    descricao: "X-Bacon",
+    preco: 14.0,
+  },
+};
+
+exports.menu = cardapio;
